be78f522a5d5acf01482af7e2f6e3fef6305b6c1
Roman <romdrak@email.cz>
2021-07-20 15:43:38 +0200

    Testing a bit bit more

--- src/neighbors.rs
+++ src/neighbors.rs
@@ -151,7 +151,8 @@ impl Neighborhood for MooreNeighborhood {
             point.1 - goal.1
         };
         
-        (((diff_0 * diff_0 + diff_1 * diff_1) as f32).sqrt() * 2.2) as usize
+        // (((diff_0 * diff_0 + diff_1 * diff_1) as f32).sqrt() * 2.2) as usize
+        diff_0 + diff_1
     }
 }
 
